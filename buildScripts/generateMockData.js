52c89222b15363b483532cb1d22defdf226215e0
Michael Vargas <Michael.Vargas@intervalintl.com>
2018-10-17 14:50:20 -0400

    Add Http Mocks For Seed

---
+++ buildScripts/generateMockData.js
@@ -0,0 +1,19 @@
+import jsf from 'json-schema-faker';
+import {schema} from './mockDataSchema';
+import chalk from 'chalk';
+import fs from 'fs';
+
+jsf.extend('faker', function() {
+    return require('faker');
+});
+
+const json = JSON.stringify(jsf(schema));
+
+fs.writeFile('./src/api/db.json', json, function(err){
+
+    if(err){
+        return console.log(chalk.red(err));
+    }else{
+        console.log(chalk.green('Mock data generated'));
+    }
+});
